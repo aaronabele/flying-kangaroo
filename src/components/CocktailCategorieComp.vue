b0eeca41de5e0dbe9e692a6e42947631edc34244
Aaron Abele <aaronabele.coding@gmail.com>
2023-03-29 18:47:07 +0200

    adds banner feat

---
+++ src/components/CocktailCategorieComp.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="cocktail-category-section">
+    <div class="cocktail-category-wrapper">
+      <div
+        class="cocktail-category-collection"
+        v-for="item in computedArr"
+        :key="item.idDrink"
+      >
+        <img
+          class="cocktail-category-image"
+          :src="item.strDrinkThumb"
+          :alt="item.strDrink"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sourceData: [],
+    };
+  },
+  computed: {
+    computedArr() {
+      return this.sourceData.slice(0, 2);
+    },
+  },
+  created() {
+    fetch("https://www.thecocktaildb.com/api/json/v2/9973533/popular.php")
+      .then((response) => response.json())
+      .then((data) => (this.sourceData = data.drinks));
+  },
+};
+</script>
+
+<style>
+.cocktail-category-section {
+  width: 1200px;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.cocktail-category-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.cocktail-category-image {
+  width: 550px;
+  height: auto;
+  position: relative;
+}
+</style>
--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -106,8 +106,10 @@ export default {};
 @import url("https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 
 #footer {
-  position: absolute;
-  bottom: 0px;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  right: 0;
   width: 100%;
   background-color: #f2f2f2;
   color: #242430;
